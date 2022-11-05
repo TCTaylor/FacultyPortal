eee9f2dd359939796cbb3dd2b18f91cd6b4eb893
TCTaylor <taylor_t02640@utpb.edu>
2022-11-05 10:42:20 -0500

    Imrpove Nav component

--- facultyportal-frontend/src/pages/navigation/navigation.component.jsx
+++ facultyportal-frontend/src/pages/navigation/navigation.component.jsx
@@ -10,7 +10,7 @@ import "./navigation.styles.css";
 
 function Navigation() {
   const [signingOut, setSigningOut] = useState(false);
-  const { displayName, isAdmin, isEditor } = useAuth();
+  const { displayName, isAdmin, isEditor, isReadOnly } = useAuth();
   const navigate = useNavigate();
 
   const handleSignOut = () => {
@@ -26,7 +26,7 @@ function Navigation() {
 
   return (
     <Fragment>
-      <div className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="navbar navbar-expand-lg bg-light">
         <div className="container-fluid">
           <Link to="/">
             <div className="navbar-brand">
@@ -37,62 +37,72 @@ function Navigation() {
             className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
-            data-bs-target="#navbarNavDropdown"
-            aria-controls="navbarNavDropdown"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link className="nav-link" to={"/courses"}>
-                <i className="bi bi-journals"> </i>
-                  Courses
-                </Link>
-              </li>
-              {(isAdmin || isEditor) && (
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              {isReadOnly && (
                 <li className="nav-item">
-                  <Link className="nav-link" to={"/"}>
-                    <i className="bi bi-people-fill"></i>
-                    Users
+                  <Link className="nav-link" to={"/courses"}>
+                    <i className="bi bi-journals"> </i>
+                    Courses
                   </Link>
                 </li>
               )}
-              <li className="nav-item dropdown">
-                <Link
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarDropdownMenuLink"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  {/* <img src="" alt="User profile picture" width="10" height="10"></img> */}
-                  <i className="bi bi-person-circle"> </i>
-                  {displayName}
-                </Link>
-                <ul
-                  className="dropdown-menu"
-                  style={{ margin: 0 }} // Fix Popper warning
-                  aria-labelledby="navbarDropdownMenuLink"
-                >
-                  <li>
-                    <Link className="dropdown-item" to="/profile">
-                      <i className="bi bi-person-circle"> </i>
-                      Profile
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="dropdown-item" onClick={handleSignOut}>
-                      <i className="bi bi-box-arrow-right"> </i>
-                      Sign Out
-                    </Link>
-                  </li>
-                </ul>
-              </li>
+              {(isAdmin || isEditor) && (
+                <li className="nav-item">
+                  <Link className="nav-link" to={"/faculty-maint"}>
+                    <i className="bi bi-people-fill"> </i>
+                    Faculty
+                  </Link>
+                </li>
+              )}
             </ul>
+            <div className="auth-dropdown d-flex">
+              <ul className="navbar-nav">
+                <li className="nav-item dropdown">
+                  <Link
+                    className="nav-link dropdown-toggle"
+                    id="navbarDropdownMenuLink"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    <img
+                      className="rounded-circle shadow-4"
+                      src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
+                      alt="Profile picture"
+                      width="60"
+                      height="60"
+                    />
+                    {" " + displayName}
+                  </Link>
+                  <ul
+                    className="dropdown-menu"
+                    style={{ margin: 0 }} // Fix Popper warning
+                    aria-labelledby="navbarDropdownMenuLink"
+                  >
+                    <li>
+                      <Link className="dropdown-item" to="/profile">
+                        <i className="bi bi-person-circle"> </i>
+                        Profile
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className="dropdown-item" onClick={handleSignOut}>
+                        <i className="bi bi-box-arrow-right"> </i>
+                        Sign Out
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
