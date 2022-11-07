23e20be2a54c5ff257209c3b074bc08ab8f9400f
TCTaylor <taylor_t02640@utpb.edu>
2022-11-06 23:11:09 -0600

    Improved sign-in

--- facultyportal-frontend/src/pages/sign-in/sign-in.component.jsx
+++ facultyportal-frontend/src/pages/sign-in/sign-in.component.jsx
@@ -1,10 +1,10 @@
 import React, { useContext, useEffect, useState } from "react";
 import UserContext from "../../context/user-context";
-import Loading from "../../components/loading/loading.component";
-import { ReactComponent as FPLogo } from "../../assets/books-stack-of-three-svgrepo-com.svg";
 import { useNavigate } from "react-router-dom";
 import { signIn } from "../../services/auth-service";
 
+import FPLogo from "../../assets/books-stack-of-three-svgrepo-com.svg";
+import SignInImg from "../../assets/CHCBuildings-2_0.webp";
 import "./sign-in.styles.css";
 
 function SignIn() {
@@ -12,6 +12,7 @@ function SignIn() {
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
   const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
 
   const { setUser } = useContext(UserContext);
   const navigate = useNavigate();
@@ -28,8 +29,13 @@ function SignIn() {
     e.preventDefault();
     setLoading(true);
     const user = await signIn(email, password);
-    setUser(user);
-    setSuccess(true);
+    if (user) {
+      setUser(user);
+      setSuccess(true);
+    } else {
+      setError(true);
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
@@ -38,49 +44,81 @@ function SignIn() {
     }
   }, [success]);
 
-  if (loading) {
-    return <Loading />;
-  }
-
   return (
     <div className="sign-in-wrapper">
-      <form className="" onSubmit={handleSignIn}>
-        <div className="sign-in-header">
-          <FPLogo className="sign-in-logo" />
-          <h1 className="sign-in-title">Faculty Portal</h1>
+      <div className="container py-5 h-100">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col col-xl-10">
+            <div className="row g-0">
+              <div className="col-md-6 col-lg-5 d-none d-md-block">
+                <img src={SignInImg} alt="Sign in form" className="img-fluid" />
+              </div>
+              <div className="col-md-6 col-lg-7 d-flex align-items-center">
+                <div className="card-body p-4 p-lg-5 text-black">
+                  <form onSubmit={handleSignIn}>
+                    <div className="d-flex align-items-center mb-3 pb-1">
+                      <img src={FPLogo} className="sign-in-logo" />
+                      <span className="h1 fw-bold mb-0">Faculty Portal</span>
+                    </div>
+                    <h5 className="fw-normal mb-3 pb-3">
+                      Sign into your account
+                    </h5>
+                    {error && (
+                      <p className="error-message">
+                        <i className="bi bi-exclamation-square-fill"> </i>
+                        Invalid email or password
+                      </p>
+                    )}
+                    <div className="username">
+                      <label>Email</label>
+                      <br />
+                      <input
+                        type="email"
+                        name="email"
+                        autoComplete="off"
+                        value={email}
+                        onChange={onChangeEmail}
+                        required
+                      />
+                    </div>
+                    <div className="password">
+                      <label>Password</label>
+                      <br />
+                      <input
+                        type="password"
+                        name="password"
+                        value={password}
+                        onChange={onChangePassword}
+                        required
+                      />
+                    </div>
+                    <div className="form-check d-flex mb-4">
+                      <input
+                        className="form-check-input me-2"
+                        type="checkbox"
+                      />
+                      <label className="form-check-label">
+                        Remember password
+                      </label>
+                    </div>
+                    <div className="pt-1 mb-4">
+                      <button className="btn btn-dark btn-lg" type="submit">
+                        {loading && (
+                          <span
+                            className="spinner-border spinner-border-sm"
+                            role="status"
+                          ></span>
+                        )}
+                        Sign In
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <h1>Sign In</h1>
-        <div className="username">
-          <label>Email</label>
-          <br />
-          <input
-            type="text"
-            name="email"
-            autoComplete="off"
-            value={email}
-            onChange={onChangeEmail}
-            required
-          />
-        </div>
-        <div className="password">
-          <label>Password</label>
-          <br />
-          <input
-            type="password"
-            name="password"
-            value={password}
-            onChange={onChangePassword}
-            required
-          />
-        </div>
-        <div className="remember-me">
-          <input type="checkbox"></input>
-          <label>Remember me?</label>
-        </div>
-        <div className="sign-in-btn">
-          <button type="submit">Sign In</button>
-        </div>
-      </form>
+      </div>
     </div>
   );
 }
